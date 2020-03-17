0ee42a440e032a8f4b64fa0ee30c6b0e530d9fe7
Devki1 <gdev3195@gmail.com>
2020-03-17 11:58:30 +0530

    ADDED:POJO  read directly csv file

--- src/main/java/com/bridgelabz/model/CSVStateCensus.java
+++ src/main/java/com/bridgelabz/model/CSVStateCensus.java
@@ -1,5 +1,49 @@
 package com.bridgelabz.model;
 
-public class CSVStateCensus {
+import com.opencsv.bean.CsvBindByName;
 
+//Binding the column name in CsvBindByName Class
+public class CSVStateCensus {
+    @CsvBindByName(column = "State")
+    public String State;
+    @CsvBindByName(column = "Population")
+    public String Population;
+    @CsvBindByName(column = "AreaInSqKm")
+    public String AreaInSqKm;
+    @CsvBindByName(column = "DensityPerSqKm")
+    public String DensityPerSqKm;
+
+    //Getter and setter method to encapsulate
+    public String getState() {
+        return State;
+    }
+
+    public void setState(String state) {
+        State = state;
+    }
+
+    public String getPopulation() {
+        return Population;
+    }
+
+    public void setPopulation(String population) {
+        Population = population;
+    }
+
+    public String getAreaInSqKm() {
+        return AreaInSqKm;
+    }
+
+    public void setAreaInSqKm(String areaInSqKm) {
+        AreaInSqKm = areaInSqKm;
+    }
+
+    public String getDensityPerSqKm() {
+        return DensityPerSqKm;
+    }
+
+    public void setDensityPerSqKm(String densityPerSqKm) {
+        DensityPerSqKm = densityPerSqKm;
+    }
 }
+
