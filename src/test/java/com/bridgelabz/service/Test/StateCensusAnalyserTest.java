d7e52a0f462495ec0ca0fc7aa3df5ab94b812802
Devki1 <gdev3195@gmail.com>
2020-03-17 12:00:54 +0530

    ADDED:Happy test case and check all the records

--- src/test/java/com/bridgelabz/service/Test/StateCensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/service/Test/StateCensusAnalyserTest.java
@@ -1,4 +1,16 @@
 package com.bridgelabz.service.Test;
 
+import com.bridgelabz.service.StateCensusAnalyser;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+
 public class StateCensusAnalyserTest {
+    @Test
+    public void givenStateCensusCSVFile_WhenTrue_NumberOfRecordShouldMatch() throws IOException {
+        StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+        int countRecord = stateCensusAnalyser.loadCensusCSVData();
+        Assert.assertEquals(countRecord, 29);
+    }
 }
