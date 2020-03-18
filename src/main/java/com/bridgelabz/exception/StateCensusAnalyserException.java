b7f117415cff3fdd0d49422857aa345824f630f2
Devki1 <gdev3195@gmail.com>
2020-03-18 09:56:24 +0530

    REFACTORED:Added ENTERED_WRONG_FILE_TYPE enum exception

--- src/main/java/com/bridgelabz/exception/StateCensusAnalyserException.java
+++ src/main/java/com/bridgelabz/exception/StateCensusAnalyserException.java
@@ -1,8 +1,11 @@
 package com.bridgelabz.exception;
 
 public class StateCensusAnalyserException extends Exception {
+    public StateCensusAnalyserException(Exceptiontype enteredWrongFileType) {
+    }
+
     public enum Exceptiontype {
-        ENTERED_WRONG_FILE;
+        ENTERED_WRONG_FILE_TYPE, INCORRECT_DELIMITER, ENTERED_WRONG_FILE;
     }
 
     public Exceptiontype type;
