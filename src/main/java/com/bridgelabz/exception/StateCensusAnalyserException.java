e2d52fc4b651f2141ea4386a6cac68b2731d921b
Devki1 <gdev3195@gmail.com>
2020-03-22 13:01:37 +0530

    REFACTORED:Added UNABLE_TO_PARSE as constant in enum

--- src/main/java/com/bridgelabz/exception/StateCensusAnalyserException.java
+++ src/main/java/com/bridgelabz/exception/StateCensusAnalyserException.java
@@ -3,8 +3,9 @@ package com.bridgelabz.exception;
 public class StateCensusAnalyserException extends Exception {
     public enum Exceptiontype {
         ENTERED_WRONG_FILE_TYPE, ENTERED_INCORRECT_DELIMITER, ENTERED_WRONG_FILE,
-         ENTERED_INCORRECT_DELIMITER_OR_HEADER ;
+        ENTERED_INCORRECT_DELIMITER_OR_HEADER, UNABLE_TO_PARSE;
     }
+
     public Exceptiontype type;
 
     public StateCensusAnalyserException(Exceptiontype type, String message) {
