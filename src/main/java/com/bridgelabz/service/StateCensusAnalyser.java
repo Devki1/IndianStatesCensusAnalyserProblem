b51c632e52c11f9754098b1d6d4d8d19e7841190
Devki1 <gdev3195@gmail.com>
2020-03-25 03:40:48 +0530

    REFACTORED:Convert Iterate method into List

--- src/main/java/com/bridgelabz/service/StateCensusAnalyser.java
+++ src/main/java/com/bridgelabz/service/StateCensusAnalyser.java
@@ -12,6 +12,7 @@ import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Iterator;
+import java.util.List;
 
 public class StateCensusAnalyser {
     int countRecord = 0;
@@ -20,14 +21,11 @@ public class StateCensusAnalyser {
     public int loadCensusCsvData(String SIMPLE_CSV_PATH) throws CSVBuilderException {
         try (Reader reader = Files.newBufferedReader(Paths.get(SIMPLE_CSV_PATH));) {
             ICSVBuilder csvBuilder = CSVBuilderFactory.createCSVBuilder();
-            Iterator<CSVStateCensus> censusCSVIterator = csvBuilder.getCSVFileIterator(reader, CSVStateCensus.class);
-            while (censusCSVIterator.hasNext()) {
-                CSVStateCensus csvStateCensus = censusCSVIterator.next();
-                countRecord++;
-            }
-            return countRecord;
+            List<CSVStateCensus> censusCSVList = csvBuilder.getCSVFileList(reader, CSVStateCensus.class);
+            return censusCSVList.size();
+
         } catch (IOException e) {
-            throw new CSVBuilderException(CSVBuilderException.Exceptiontype.ENTERED_WRONG_FILE_TYPE, e.getMessage());
+            throw new CSVBuilderException(CSVBuilderException.Exceptiontype.ENTERED_WRONG_FILE, e.getMessage());
         } catch (RuntimeException e) {
             throw new CSVBuilderException
                     (CSVBuilderException.Exceptiontype.ENTERED_INCORRECT_DELIMITER_OR_HEADER, e.getMessage());
@@ -37,13 +35,8 @@ public class StateCensusAnalyser {
     public int loadSateCodeCsvData(String CSV_PATH) throws CSVBuilderException {
         try (Reader reader = Files.newBufferedReader(Paths.get(CSV_PATH));) {
             ICSVBuilder csvBuilder = CSVBuilderFactory.createCSVBuilder();
-            Iterator<CSVState> csvStateIterator = csvBuilder.getCSVFileIterator(reader, CSVState.class);
-
-            while (csvStateIterator.hasNext()) {
-                countRecord++;
-                CSVState csvState = csvStateIterator.next();
-            }
-            return countRecord;
+            List<CSVState> censusCSVList = csvBuilder.getCSVFileList(reader, CSVState.class);
+            return censusCSVList.size();
         } catch (IOException e) {
             throw new CSVBuilderException(CSVBuilderException.Exceptiontype.ENTERED_WRONG_FILE, e.getMessage());
         } catch (RuntimeException e) {
