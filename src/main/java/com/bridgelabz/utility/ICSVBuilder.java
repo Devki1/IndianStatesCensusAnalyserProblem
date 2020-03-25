8c32dc354b86a39cc95486e2f50e797872c7c7d7
Devki1 <gdev3195@gmail.com>
2020-03-25 15:30:33 +0530

    REFACTORED:Convert Iterator method into List

--- src/main/java/com/bridgelabz/utility/ICSVBuilder.java
+++ src/main/java/com/bridgelabz/utility/ICSVBuilder.java
@@ -6,7 +6,9 @@ import com.bridgelabz.model.CSVStateCensus;
 
 import java.io.Reader;
 import java.util.Iterator;
+import java.util.List;
 
 public interface ICSVBuilder {
-    public <E> Iterator<E> getCSVFileIterator(Reader reader, Class<E> csvClass) throws StateCensusAnalyserException;
+    public <E> Iterator<E> getCSVFileIterator(Reader reader, Class<E> csvClass) throws  CSVBuilderException;
+    public <E> List<E> getCSVFileList(Reader reader, Class<E> csvClass) throws CSVBuilderException;
 }
