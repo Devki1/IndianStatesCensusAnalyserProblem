979ae06b8f51db7d002594c9bd9e5d233dd1381f
Devki1 <gdev3195@gmail.com>
2020-03-26 10:52:05 +0530

    ADDED:Method for sorting list data and return data in json format

--- src/main/java/com/bridgelabz/service/StateCensusAnalyser.java
+++ src/main/java/com/bridgelabz/service/StateCensusAnalyser.java
@@ -5,38 +5,38 @@ import com.bridgelabz.model.CSVState;
 import com.bridgelabz.model.CSVStateCensus;
 import com.bridgelabz.utility.CSVBuilderFactory;
 import com.bridgelabz.utility.ICSVBuilder;
+import com.google.gson.Gson;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Iterator;
+import java.util.Comparator;
 import java.util.List;
 
 public class StateCensusAnalyser {
-    int countRecord = 0;
+    List<CSVStateCensus> stateCensusRecords = null;
+    List<CSVState> stateCensusCodesRecord = null;
 
     //Reading and printing data from csv file
-    public int loadCensusCsvData(String SIMPLE_CSV_PATH) throws CSVBuilderException {
-        try (Reader reader = Files.newBufferedReader(Paths.get(SIMPLE_CSV_PATH));) {
+    public int loadCensusCsvData(String SAMPLE_CSV_PATH) throws CSVBuilderException {
+        try (Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_PATH));) {
             ICSVBuilder csvBuilder = CSVBuilderFactory.createCSVBuilder();
-            List<CSVStateCensus> censusCSVList = csvBuilder.getCSVFileList(reader, CSVStateCensus.class);
-            return censusCSVList.size();
-
+            stateCensusRecords = csvBuilder.getCSVFileList(reader, CSVStateCensus.class);
+            return stateCensusRecords.size();
         } catch (IOException e) {
             throw new CSVBuilderException(CSVBuilderException.Exceptiontype.ENTERED_WRONG_FILE, e.getMessage());
         } catch (RuntimeException e) {
-            throw new CSVBuilderException
-                    (CSVBuilderException.Exceptiontype.ENTERED_INCORRECT_DELIMITER_OR_HEADER, e.getMessage());
+            throw new CSVBuilderException(CSVBuilderException.Exceptiontype.ENTERED_INCORRECT_DELIMITER_OR_HEADER, e.getMessage());
         }
     }
 
     public int loadSateCodeCsvData(String CSV_PATH) throws CSVBuilderException {
         try (Reader reader = Files.newBufferedReader(Paths.get(CSV_PATH));) {
             ICSVBuilder csvBuilder = CSVBuilderFactory.createCSVBuilder();
-            List<CSVState> censusCSVList = csvBuilder.getCSVFileList(reader, CSVState.class);
-            return censusCSVList.size();
+            stateCensusCodesRecord = csvBuilder.getCSVFileList(reader, CSVState.class);
+            return stateCensusCodesRecord.size();
         } catch (IOException e) {
             throw new CSVBuilderException(CSVBuilderException.Exceptiontype.ENTERED_WRONG_FILE, e.getMessage());
         } catch (RuntimeException e) {
@@ -55,6 +55,28 @@ public class StateCensusAnalyser {
             }
         }
     }
+
+    //Sorting in a  JSON formats
+    public String getStateWiseSortedCensusData() throws CSVBuilderException {
+        if (stateCensusRecords == null || stateCensusRecords.size() == 0)
+            throw new CSVBuilderException(CSVBuilderException.Exceptiontype.NO_CENSUS_DATA, "NO_CENSUS_DATA");
+        Comparator<CSVStateCensus> censusCSVComparator = Comparator.comparing(csvStateCensus -> csvStateCensus.getState());
+        this.sort(censusCSVComparator);
+        String sortedStateCensusJson = new Gson().toJson(stateCensusRecords);
+        return sortedStateCensusJson;
+    }
+
+    //Sorting CSV State  census data
+    public void sort(Comparator<CSVStateCensus> censusCSVComparator) {
+        for (int iterate = 0; iterate < stateCensusRecords.size() - 1; iterate++) {
+            for (int Inneriterate = 0; Inneriterate < stateCensusRecords.size() - iterate - 1; Inneriterate++) {
+                CSVStateCensus census1 = stateCensusRecords.get(Inneriterate);
+                CSVStateCensus census2 = stateCensusRecords.get(Inneriterate + 1);
+                if (censusCSVComparator.compare(census1, census2) > 0) {
+                    stateCensusRecords.set(Inneriterate, census2);
+                    stateCensusRecords.set(Inneriterate + 1, census1);
+                }
+            }
+        }
+    }
 }
-
-
