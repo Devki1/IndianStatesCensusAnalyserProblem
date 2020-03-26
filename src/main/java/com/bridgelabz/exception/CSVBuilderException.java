5bae3705ea4c7e6e49bb2236d7606ccea0d0bdf9
Devki1 <gdev3195@gmail.com>
2020-03-26 12:52:50 +0530

    ADDED:Exception type NO_CENSUS_DATA

--- src/main/java/com/bridgelabz/exception/CSVBuilderException.java
+++ src/main/java/com/bridgelabz/exception/CSVBuilderException.java
@@ -3,7 +3,7 @@ package com.bridgelabz.exception;
 public class CSVBuilderException extends Exception{
     public enum Exceptiontype {
          ENTERED_WRONG_FILE,ENTERED_WRONG_FILE_TYPE,
-        ENTERED_INCORRECT_DELIMITER_OR_HEADER,ILLEGAL_STATE;
+        ENTERED_INCORRECT_DELIMITER_OR_HEADER,ILLEGAL_STATE,NO_CENSUS_DATA;
     }
     public Exceptiontype type;
 
