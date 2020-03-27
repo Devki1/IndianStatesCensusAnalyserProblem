44320212bae74fc25b263046bff0faa54f40ef62
Devki1 <gdev3195@gmail.com>
2020-03-27 20:31:37 +0530

    Created a map of state and census data instead of list

--- src/main/java/com/bridgelabz/utility/ICSVBuilder.java
+++ src/main/java/com/bridgelabz/utility/ICSVBuilder.java
@@ -1,14 +1,16 @@
 package com.bridgelabz.utility;
 
 import com.bridgelabz.exception.CSVBuilderException;
-import com.bridgelabz.exception.StateCensusAnalyserException;
-import com.bridgelabz.model.CSVStateCensus;
 
 import java.io.Reader;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 
 public interface ICSVBuilder {
-    public <E> Iterator<E> getCSVFileIterator(Reader reader, Class<E> csvClass) throws  CSVBuilderException;
-    public <E> List<E> getCSVFileList(Reader reader, Class<E> csvClass) throws CSVBuilderException;
+    public <E> Iterator<E> getCSVFileIterator(Reader reader, Class<E> csvClass) throws CSVBuilderException;
+
+    public <E> List getCSVFileList(Reader reader, Class<E> csvClass) throws CSVBuilderException;
+
+    public <E> HashMap<E, E> getCSVFileMap(Reader reader, Class csvClass) throws CSVBuilderException;
 }
