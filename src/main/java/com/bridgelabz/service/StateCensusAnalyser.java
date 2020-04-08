0953d666f2894821759b446f931f3078b2403f75
Devki1 <gdev3195@gmail.com>
2020-04-09 00:24:53 +0530

    REFACTORED:Adapter pattern to accommodate DRY principle which was violated

--- src/main/java/com/bridgelabz/service/StateCensusAnalyser.java
+++ src/main/java/com/bridgelabz/service/StateCensusAnalyser.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.service;
 
+import com.bridgelabz.adaptor.CensusAdapter;
+import com.bridgelabz.adaptor.CensusAdapterFactory;
 import com.bridgelabz.dao.CensusDAO;
 import com.bridgelabz.exception.CSVBuilderException;
 import com.bridgelabz.utility.CensusLoader;
@@ -10,18 +12,17 @@ import java.io.IOException;
 import java.util.*;
 
 public class StateCensusAnalyser {
-
     CensusLoader censusLoader = new CensusLoader();
     Collection<CensusDAO> censusRecords = null;
-    HashMap<String, CensusDAO> censusDAOMap = new HashMap<String, CensusDAO>();
+    Map<String, CensusDAO> censusDAOMap = new HashMap<String, CensusDAO>();
 
-    public int loadCensusData(String... csvFilePath) throws CSVBuilderException {
-        censusDAOMap = censusLoader.loadStateCensusData(censusDAOMap, csvFilePath);
-        return censusDAOMap.size();
-    }
 
-    public int loadUSCensusData(String... csvFilePath) throws CSVBuilderException {
-        censusDAOMap = censusLoader.loadUSCensusData(censusDAOMap, csvFilePath);
+    public enum COUNTRY {INDIA, US}
+
+    //Generic method load csv file data
+    public int loadCensusData(COUNTRY country, String... csvFilePath) throws CSVBuilderException, IOException {
+        CensusAdapter censusDataLoader = CensusAdapterFactory.getCensusData(country);
+        censusDAOMap = censusDataLoader.loadCensusData(csvFilePath);
         return censusDAOMap.size();
     }
 
@@ -38,6 +39,7 @@ public class StateCensusAnalyser {
         }
     }
 
+    //Sorting in a JSON format to StateCensus data
     public String getStateWiseSortedCensusData() throws CSVBuilderException {
         if (censusDAOMap == null || censusDAOMap.size() == 0)
             throw new CSVBuilderException(CSVBuilderException.Exceptiontype.NO_CENSUS_DATA, "No data found");
@@ -49,6 +51,7 @@ public class StateCensusAnalyser {
         return sortedStateCensusJson;
     }
 
+    //Sorting in a JSON format to StateCodeData
     public String getStateCodeWiseSortedData() throws CSVBuilderException {
         if (censusDAOMap == null || censusDAOMap.size() == 0)
             throw new CSVBuilderException(CSVBuilderException.Exceptiontype.NO_CENSUS_DATA, "No data found");
@@ -60,6 +63,7 @@ public class StateCensusAnalyser {
         return sortedStateCodeJson;
     }
 
+    //Sorting csv state census data population wise
     public String getStateCensusPopulationWiseSortedData() throws CSVBuilderException {
         if (censusDAOMap == null || censusDAOMap.size() == 0)
             throw new CSVBuilderException(CSVBuilderException.Exceptiontype.NO_CENSUS_DATA, "No data found");
@@ -72,6 +76,7 @@ public class StateCensusAnalyser {
         return sortedStateCensusPopulationJson;
     }
 
+    //Sorting csv state census data density wise
     public String getStateCensusPopulationDensityWiseSortedData() throws CSVBuilderException {
         if (censusDAOMap == null || censusDAOMap.size() == 0)
             throw new CSVBuilderException(CSVBuilderException.Exceptiontype.NO_CENSUS_DATA, "No data found");
@@ -84,6 +89,7 @@ public class StateCensusAnalyser {
         return sortedStateCensusPopulationJson;
     }
 
+    //Sorting csv State Census data wise
     public String getStateCensusLargestAreaWiseSortedData() throws CSVBuilderException {
         if (censusDAOMap == null || censusDAOMap.size() == 0)
             throw new CSVBuilderException(CSVBuilderException.Exceptiontype.NO_CENSUS_DATA, "No data found");
@@ -96,6 +102,7 @@ public class StateCensusAnalyser {
         return sortedStateCensusPopulationJson;
     }
 
+    //Sorting csv file generic method
     private <E extends CensusDAO> LinkedHashMap<String, CensusDAO> sort(Comparator censusComparator) {
         Set<Map.Entry<String, CensusDAO>> entries = censusDAOMap.entrySet();
         List<Map.Entry<String, CensusDAO>> listOfEntries =
@@ -103,6 +110,7 @@ public class StateCensusAnalyser {
         Collections.sort(listOfEntries, censusComparator);
         LinkedHashMap<String, CensusDAO> sortedByValue =
                 new LinkedHashMap<String, CensusDAO>(listOfEntries.size());
+        // Coping list to map
         for (Map.Entry<String, CensusDAO> entry : listOfEntries) {
             sortedByValue.put(entry.getKey(), entry.getValue());
         }
