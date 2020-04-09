fb0abcebbdb56e116fca228f8b8ed7fc807bb43b
Devki1 <gdev3195@gmail.com>
2020-04-09 13:43:32 +0530

    REFACTORED:Used DAO and DTO to pattern solve

--- src/main/java/com/bridgelabz/model/USCensus.java
+++ src/main/java/com/bridgelabz/model/USCensus.java
@@ -4,58 +4,39 @@ import com.opencsv.bean.CsvBindByName;
 
 public class USCensus {
     @CsvBindByName(column = "State Id")
-    private String stateID;
+    public String stateID;
 
     @CsvBindByName(column = "State")
-    private String state;
+    public String state;
 
     @CsvBindByName(column = "Population")
-    private Long population;
+    public Long population;
 
     @CsvBindByName(column = "Total area")
-    private Double area;
+    public Double area;
 
     @CsvBindByName(column = "Population Density")
-    private Float populationDensity;
+    public Float populationDensity;
 
-    public void setStateID(String stateID) {
-        this.stateID = stateID;
+    public USCensus() {
     }
 
-    public String getStateID() {
-        return stateID;
-    }
-
-    public void setState(String state) {
+    public USCensus(String state, String stateCode, Long population, Double areaInSqrKm, Float DensityPerSqKm) {
+        this.stateID = stateCode;
         this.state = state;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setPopulation(Long population) {
         this.population = population;
+        this.area = areaInSqrKm;
+        this.populationDensity = DensityPerSqKm;
     }
 
-    public Long getPopulation() {
-        return population;
-    }
-
-    public void setArea(Double area) {
-        this.area = area;
-    }
-
-    public Double getArea() {
-        return area;
-    }
-
-    public void setPopulationDensity(Float populationDensity) {
-        this.populationDensity = populationDensity;
-    }
-
-    public Float getPopulationDensity() {
-        return populationDensity;
+    @Override
+    public String toString() {
+        return "USCensus{" +
+                "stateID='" + stateID + '\'' +
+                ", state='" + state + '\'' +
+                ", population=" + population +
+                ", area=" + area +
+                ", populationDensity='" + populationDensity + '\'' +
+                '}';
     }
 }
-
