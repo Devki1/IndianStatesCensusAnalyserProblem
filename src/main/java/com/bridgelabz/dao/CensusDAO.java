7229ffb7b9477270de6abc2d93cc1cec5896a496
Devki1 <gdev3195@gmail.com>
2020-04-09 13:38:17 +0530

    REFACTORED:Used DAO and DTO Pattern to solve

--- src/main/java/com/bridgelabz/dao/CensusDAO.java
+++ src/main/java/com/bridgelabz/dao/CensusDAO.java
@@ -1,41 +1,80 @@
 package com.bridgelabz.dao;
 
-import com.bridgelabz.model.CSVState;
-import com.bridgelabz.model.CSVStateCensus;
+//import com.bridgelabz.model.CSVStateCensus;
+//port com.bridgelabz.model.CSVStatesCode;
+
+import com.bridgelabz.model.IndianStateCensusCSV;
+import com.bridgelabz.model.IndianStateCodeCSV;
 import com.bridgelabz.model.USCensus;
+import com.bridgelabz.service.CensusAnalyser;
+
+import java.util.Comparator;
 
 public class CensusDAO {
     public String state;
-    public long areaInSqKm;
-    public Integer densityPerSqKm;
+    public Double areaInSqKm;
+    public Float densityPerSqKm;
     public Long population;
-    public String stateName;
     public String stateCode;
     public Integer srNo;
     public Integer tin;
-    public String stateID;
-    public Double area;
-    public Float populationDensity;
 
-    public CensusDAO(CSVStateCensus csvStateCensus) {
-        state = csvStateCensus.getState();
-        areaInSqKm = (long) csvStateCensus.getAreaInSqKm();
-        densityPerSqKm = csvStateCensus.getDensityPerSqkm();
-        population = csvStateCensus.getPopulation();
+    public CensusDAO(IndianStateCensusCSV indianStateCensusCSV) {
+        state = indianStateCensusCSV.state;
+        areaInSqKm = indianStateCensusCSV.areaInSqKm;
+        densityPerSqKm = indianStateCensusCSV.densityPerSqkm;
+        population = indianStateCensusCSV.population;
     }
 
-    public CensusDAO(CSVState csvStateCode) {
-        state = csvStateCode.getStateName();
-        stateCode = csvStateCode.getStateCode();
-        srNo = csvStateCode.getSrNo();
-        tin = csvStateCode.getTIN();
+    public CensusDAO(IndianStateCodeCSV csvStateCode) {
+        state = csvStateCode.stateName;
+        stateCode = csvStateCode.stateCode;
+        srNo = csvStateCode.srNo;
+        tin = csvStateCode.tin;
     }
+
     public CensusDAO(USCensus usCensus) {
-        stateID = usCensus.getStateID();
-        state = usCensus.getState();
-        population = usCensus.getPopulation();
-        area = usCensus.getArea();
-        populationDensity = usCensus.getPopulationDensity();
+        stateCode = usCensus.stateID;
+        state = usCensus.state;
+        population = usCensus.population;
+        areaInSqKm = usCensus.area;
+        densityPerSqKm = usCensus.populationDensity;
+    }
+
+    public CensusDAO() {
+    }
+
+    public static Comparator<? super CensusDAO> getSortComparator(CensusAnalyser.SortingMode mode) {
+        if (mode.equals(CensusAnalyser.SortingMode.STATE))
+            return Comparator.comparing(census -> census.state);
+        if (mode.equals(CensusAnalyser.SortingMode.STATECODE))
+            return Comparator.comparing(census -> census.stateCode);
+        if (mode.equals(CensusAnalyser.SortingMode.POPULATION))
+            return Comparator.comparing(CensusDAO::getPopulation).reversed();
+        if (mode.equals(CensusAnalyser.SortingMode.DENSITY))
+            return Comparator.comparing(CensusDAO::getPopulationDensity).reversed();
+        if (mode.equals(CensusAnalyser.SortingMode.AREA))
+            return Comparator.comparing(CensusDAO::getTotalArea).reversed();
+        return null;
+    }
+
+    private Double getTotalArea() {
+        return areaInSqKm;
+    }
+
+    private Float getPopulationDensity() {
+        return this.densityPerSqKm;
+    }
+
+    private Long getPopulation() {
+        return this.population;
+    }
+
+    public Object getCensusDTO(CensusAnalyser.COUNTRY country) {
+        if (country.equals(CensusAnalyser.COUNTRY.INDIA))
+            return new IndianStateCensusCSV(state, stateCode, population, areaInSqKm, densityPerSqKm);
+        if (country.equals(CensusAnalyser.COUNTRY.US))
+            return new USCensus(state, stateCode, population, areaInSqKm, densityPerSqKm);
+        return null;
     }
 }
-
